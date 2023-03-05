eac001cfcb1a200d68c90e23ff3133c79df85bc4
37 <urarav@37.com>
2023-03-05 22:49:51 +0800

    'cache'

--- utils/sign.js
+++ utils/sign.js
@@ -2,6 +2,7 @@ const { APP_ID, APP_SECRET } = require("../config/index");
 const instance = require("./request");
 const { getRandomStr, getTimpstamp, parseObj } = require("./common");
 const sha1 = require("sha1");
+const db = require("../db/index");
 
 async function getAccessToken() {
   const {
@@ -22,6 +23,43 @@ async function getJsApiTicket() {
   return ticket;
 }
 
+function cache() {
+  return new Promise((resolve, reject) => {
+    db.query("select * from cache_table", async (err, results) => {
+      if (err) reject(err);
+      if (results.length) {
+        const [{ ticket, ticket_time }] = results;
+        if (Date.now() - ticket_time > 7000 * 1000) {
+          const ticket = await getJsApiTicket();
+          db.query(
+            "update cache_table set ticket=?, ticket_time=? where ticket=?",
+            [ticket, String(Date.now()), ticket],
+            (err, results) => {
+              if (err) reject(err);
+              if (results.affectedRows === 1) resolve(ticket);
+            }
+          );
+        } else {
+          resolve(ticket);
+        }
+      } else {
+        const ticket = await getJsApiTicket();
+        db.query(
+          "insert into cache_table set ?",
+          {
+            ticket,
+            ticket_time: String(Date.now()),
+          },
+          (err, results) => {
+            if (err) reject(err);
+            if (results.affectedRows === 1) resolve(ticket);
+          }
+        );
+      }
+    });
+  });
+}
+
 /**
  * 1）参与签名的字段包括noncestr（随机字符串）, 有效的jsapi_ticket, timestamp（时间戳）, url（当前网页的URL，不包含#及其后面部分）
  * 2）对所有待签名参数按照字段名的ASCII 码从小到大排序（字典序）后
@@ -29,8 +67,8 @@ async function getJsApiTicket() {
  * 4）对string1作sha1加密，字段名和字段值都采用原始值，不进行URL 转义
  */
 async function sign(url) {
+  const jsapi_ticket = await cache();
   const _url = decodeURIComponent(url);
-  const jsapi_ticket = await getJsApiTicket();
   const obj = {
     noncestr: getRandomStr(),
     jsapi_ticket,
